41db0c4726991e8e9ff17e9cc2ff979ccd635f55
chenkezhun <61738196+chenkezhun@users.noreply.github.com>
2022-05-08 13:16:23 +0800

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,17 @@
+alert("Hello World!");
+var num = 1;
+num = 3;
+var price = 1.5;
+var myName = 'price';
+var trueValue = true;
+var nullVar = null;
+var und;
+
+console.log("num: " + num);
+console.log("myNmae: " + myName);
+console.log("trueValue: " + trueValue);
+console.log("price: " + price);
+console.log("nullVar: " + nullVar);
+console.log("und: " + und);
+
+document.write("nullVar: " + nullVar);
